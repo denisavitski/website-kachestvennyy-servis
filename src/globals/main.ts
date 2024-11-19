5b25e4f39b3c89b11a2732e7a4b2d5a7f8a0249c
denisavitski <denisavitski@gmail.com>
2024-11-19 17:36:19 +0300

    feat: header

---
+++ src/globals/main.ts
@@ -0,0 +1,17 @@
+import 'aptechka/accordion'
+import 'aptechka/popover'
+import 'aptechka/custom-scrollbar'
+import 'aptechka/slicer'
+import 'aptechka/scroll'
+import 'aptechka/iframe'
+import 'aptechka/masonry-layout'
+import 'aptechka/size-element'
+
+import '@globals/main.css'
+
+import '@lib/SegmentElement'
+import '@lib/ImageCloak'
+import '@lib/SwipePopoverElement'
+
+import './dev'
+import './page'
---
+++ src/globals/page.ts
@@ -0,0 +1,42 @@
+import { Morph } from 'aptechka/morph'
+
+export const morph = new Morph({
+  trailingSlash: true,
+})
+
+function updateSize() {
+  const clientWidth = document.documentElement.clientWidth
+
+  document.documentElement.style.setProperty(
+    '--inner-height',
+    innerHeight + 'px',
+  )
+
+  document.documentElement.style.setProperty(
+    '--client-width',
+    clientWidth + 'px',
+  )
+}
+
+function scrollListener() {
+  if (morph.scrollElement.scrollTop > 0) {
+    document.documentElement.classList.add('scrolled')
+  } else {
+    document.documentElement.classList.remove('scrolled')
+  }
+}
+
+document.addEventListener('morphStart', () => {
+  morph.scrollElement.removeEventListener('scroll', scrollListener)
+})
+
+document.addEventListener('morphComplete', () => {
+  morph.scrollElement.addEventListener('scroll', scrollListener)
+})
+
+addEventListener('load', () => {
+  document.documentElement.classList.add('page-loaded')
+  updateSize()
+})
+
+addEventListener('resize', updateSize)
